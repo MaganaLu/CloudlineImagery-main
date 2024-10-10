ee1dc802fbf6a5134dccfc9b3fa23b224c58dfa4
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-10 09:48:52 -0700

    ..

--- CraigDrone/public/main.js
+++ CraigDrone/public/main.js
@@ -9,7 +9,8 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 
 const dirPath = join(__dirname, "../portfolioEntries");
 const dirPathServices = join(__dirname, "../services");
-const dirPathPages = join(__dirname, "../src/pages/content");
+const dirPathHome = join(__dirname, "../home");
+
 let portfolioEntriesList = [];
 let servicesEntriesList =[];
 let pagelist = [];
@@ -166,25 +167,63 @@ const getServicesEntries = () => {
     return
 }
 
-const getPages = () => {
-    readdir(dirPathPages, (err, files) => {
+const getHomeEntries = () => {
+    readdir(dirPathHome, (err, files) => {
         if (err) {
             return console.log("Failed to list contents of directory: " + err)
         }
+        let ilist = []
         files.forEach((file, i) => {
-            let page
-            readFile(`${dirPathPages}/${file}`, "utf8", (err, contents) => {
-                page = {
-                    content: contents
+            let obj = {}
+            let post
+            readFile(`${dirPathHome}/${file}`, "utf8", (err, contents) => {
+                const getMetadataIndices = (acc, elem, i) => {
+                    if (/^---/.test(elem)) {
+                        acc.push(i)
+                    }
+                    return acc
                 }
-                pagelist.push(page)
-                let data = JSON.stringify(pagelist)
-                writeFileSync("src/pages.json", data)
+                const parseMetadata = ({ lines, metadataIndices }) => {
+                    if (metadataIndices.length > 0) {
+                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
+                        metadata.forEach(line => {
+                            obj[line.split(": ")[0]] = line.split(": ")[1]
+                        })
+                        return obj
+                    }
+                }
+                const lines = contents.split("\n")
+                const metadataIndices = lines.reduce(getMetadataIndices, [])
+                const metadata = parseMetadata({ lines, metadataIndices })
+                const parsedDate = metadata.date ? formatDate(metadata.date) : new Date()
+                const datestring = `${parsedDate["year"]}-${parsedDate["month"]}-${parsedDate["day"]}T${parsedDate["time"]}:00`
+                const date = new Date(datestring)
+                const timestamp = date.getTime() / 1000;
+
+                post = {
+                    id: timestamp,
+                    title: metadata.title ? metadata.title : "No title given",
+                    subtext: metadata.subtext ? metadata.subtext : "No subtext given",
+                    mediaURL: metadata.mediaURL ? metadata.mediaURL : "No media given",
+                }
+                servicesEntriesList.push(post)
+                ilist.push(i)
+                if (ilist.length === files.length) {
+                    const sortedList = servicesEntriesList.sort((a, b) => {
+                        return a.id < b.id ? 1 : -1
+                    })
+                    let data = JSON.stringify(sortedList)
+                    writeFileSync("src/homeEntries.json", data)
+                    
+                    console.log("home: ",data);
+                }
             })
         })
     })
     return
 }
 
+
 getPortfolioEntries();
 getServicesEntries();
+getHomeEntries();
