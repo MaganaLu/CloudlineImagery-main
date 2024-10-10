ab72d1a7b23b849a41be5050582dccc7def3cb82
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-10 11:07:23 -0700

    portfolio section fixed

--- CraigDrone/src/pages/Portfolio.jsx
+++ CraigDrone/src/pages/Portfolio.jsx
@@ -8,38 +8,47 @@ import React, { useState, useEffect } from "react";
 
 import portfolioEntries from "../portfolioEntries.json";
 
-const Portfolio = () => {
-  const [golfCourseEntries, setGolfCourseEntries] = useState([]);
-  const [realEstateEntries, setRealEstateEntries] = useState([]);
 
-  //constrcution , event coverage, other
-  const [constructionEntries, setConstructionEntries] = useState([]);
-  const [eventEntries, setEventEntries] = useState([]);
-  const [otherEntries, setOtherEntries] = useState([]);
+const Portfolio = () => {
+  var sorted = {};
+
+
+  for (var i = 0, max = portfolioEntries.length; i < max; i++) {
+
+    if (sorted[portfolioEntries[i].type] == undefined) {
+      sorted[portfolioEntries[i].type] = [];
+    }
+    sorted[portfolioEntries[i].type].push(portfolioEntries[i]);
+  }
 
   useEffect(() => {
-
-
     const urlHash = window.location.hash;
-    
     if (urlHash.length) {
       const element = document.getElementById(urlHash);
       if (element) {
         setTimeout(function () {
-        element.scrollIntoView({
-          behavior:"smooth",
-          block: "start",
-        });
-      },250);
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }, 250);
       }
-
-      
     }
-
     return () => {
-      
     };
   }, [])
+
+function checkUndefinedSection(arrayString) {
+  if (sorted[arrayString] != undefined) {
+    
+    return (sorted[arrayString].map(portfolioEntry =>
+      <ul key={portfolioEntry.title} className="ulPortfolio">
+        <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.videoURL} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
+      </ul>)
+    )
+  }
+
+}
 
 
   return (
@@ -47,53 +56,31 @@ const Portfolio = () => {
 
       <PageHeader image='../assets/NatureImage.jpg' hText="Portfolio" />
 
-      <hr id='#Golf-Courses' className="hr-text" data-content="Golf Courses"/>
+      <hr id='#Golf-Courses' className="hr-text" data-content="Golf Courses" />
       <div className="portfolioCardContainer">
-        {portfolioEntries.map(portfolioEntry =>
-          <ul key={portfolioEntry.title} className="ulPortfolio">
-            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.videoURL} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
-          </ul>
-        )}
+        {checkUndefinedSection("Golf Courses\r")}
       </div>
 
-{/*
-      <hr id='#Real-Estate' className="hr-text" data-content="Real Estate"/>
+
+      <hr id='#Real-Estate' className="hr-text" data-content="Real Estate" />
       <div className="portfolioCardContainer">
-        {realEstateEntries.map(portfolioEntry =>
-          <ul key={portfolioEntry.title} className="ulPortfolio">
-            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
-          </ul>
-        )}
+        {checkUndefinedSection("Real Estate\r")}
       </div>
 
-      <hr id='#Construction' className="hr-text" data-content="Construction"/>
+      <hr id='#Construction' className="hr-text" data-content="Construction" />
       <div className="portfolioCardContainer">
-        {constructionEntries.map(portfolioEntry =>
-          <ul key={portfolioEntry.title} className="ulPortfolio">
-            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
-          </ul>
-        )}
+        {checkUndefinedSection("Construction")}
       </div>
 
-      <hr id='#Event-Coverage' className="hr-text" data-content="Event Coverage"/>
+      <hr id='#Event-Coverage' className="hr-text" data-content="Event Coverage" />
       <div className="portfolioCardContainer">
-        {eventEntries.map(portfolioEntry =>
-          <ul key={portfolioEntry.title} className="ulPortfolio">
-            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
-          </ul>
-        )}
+        {checkUndefinedSection("Events\r")}
       </div>
 
-      <hr id='#Other-Services' className="hr-text" data-content="Other Services"/>
+      <hr id='#Other-Services' className="hr-text" data-content="Other Services" />
       <div className="portfolioCardContainer">
-        {otherEntries.map(portfolioEntry =>
-          <ul key={portfolioEntry.title} className="ulPortfolio">
-            <PortfolioCardComponent description={portfolioEntry.description} video={portfolioEntry.video_url} title={portfolioEntry.title} date={portfolioEntry.date} image={portfolioEntry.image} type={portfolioEntry.type} />
-          </ul>
-        )}
+        {checkUndefinedSection("Other\r")}
       </div>
-*/}
-
 
     </>
   )
