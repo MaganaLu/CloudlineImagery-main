c8e3cfb0c39235aa4f2403bde9aed7d5d99d42dd
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-19 08:25:24 -0700

    props.thumbnail

--- CraigDrone/src/components/PortfolioCardComponent.jsx
+++ CraigDrone/src/components/PortfolioCardComponent.jsx
@@ -6,7 +6,6 @@ const PortfolioCardComponent = (props) => {
     console.log("description", props.description);
     console.log("thumbnail: ", props.thumbnail)
     
-    let imageName = require(props.thumbnail);
 
     //let dateString = (props.date.toDate().toDateString()).toString();
     //console.log("props.thumbnail",props.thumbnail);
@@ -30,7 +29,7 @@ const PortfolioCardComponent = (props) => {
 
                     <p class="date">{props.date}</p>
                 </div>
-                <img id='portfolioVideoPlayer' alt="Image Not Found" src={imageName.default}></img>
+                <img id='portfolioVideoPlayer' alt="Image Not Found" src={props.thumbnail}></img>
             </div>
         </div>
     )
