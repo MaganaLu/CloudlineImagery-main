0eceb02b2f539f2545a71b7d394efdd50b5b4e79
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-18 22:56:38 -0700

    ...

--- CraigDrone/src/components/PortfolioCardComponent.jsx
+++ CraigDrone/src/components/PortfolioCardComponent.jsx
@@ -1,6 +1,7 @@
 import './PortfolioCardComponent.css'
 import React, { Component } from 'react'; 
 import img from '../images/dji_20240921144245_0040_d-2.jpg'
+import img2 from '../../public/images/dji_20240921144245_0040_d-2.jpg'
 
 const PortfolioCardComponent = (props) => {
 
@@ -14,7 +15,7 @@ const PortfolioCardComponent = (props) => {
             return (<iframe id='portfolioVideoPlayer' src={videoString + "?autoplay=false&loop=true&muted=false&preload=true&responsive=true"} allowFullScreen="true" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" ></iframe>);
         }
             */}
-        return ( <img id='portfolioVideoPlayer' alt="image" src={require('../images/dji_20240921144245_0040_d-2.jpg')}></img>);
+        return ( <img id='portfolioVideoPlayer' alt="image" src={'../../public/images/dji_20240921144245_0040_d-2.jpg'}></img>);
     }
 
     return (
