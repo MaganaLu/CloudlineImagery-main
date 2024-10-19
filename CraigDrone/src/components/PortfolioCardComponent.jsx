ba002fe24e1baec2c4726519049401b1b6be1ee9
Luis Magana <49489590+MaganaLu@users.noreply.github.com>
2024-10-19 10:27:45 -0700

    check if undefined urls

--- CraigDrone/src/components/PortfolioCardComponent.jsx
+++ CraigDrone/src/components/PortfolioCardComponent.jsx
@@ -8,18 +8,23 @@ const PortfolioCardComponent = (props) => {
     //console.log("props.thumbnail",props.thumbnail);
     const isIframe = () => {
         //bunnyCDN video
-        if ((props.videoLink).includes("iframe")) {
-            let bunnyVideoString = (props.videoLink).replace("play", "embed");
+        if (props.videoLink != undefined) {
+            if ((props.videoLink).includes("iframe")) {
+                let bunnyVideoString = (props.videoLink).replace("play", "embed");
 
-            return (<iframe id='portfolioVideoPlayer' src={bunnyVideoString + "?autoplay=false&loop=true&muted=false&preload=true&responsive=true"} allowFullScreen="true" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" ></iframe>);
+                return (<iframe id='portfolioVideoPlayer' src={bunnyVideoString + "?autoplay=false&loop=true&muted=false&preload=true&responsive=true"} allowFullScreen="true" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" ></iframe>);
+            }
+            //could be a youtibe video 
+            if ((props.videoLink).includes("youtube")) {
+                let youtubeVideoString = (props.videoLink).replace("watch?v=", "embed/");
+                return (<iframe id='portfolioVideoPlayer' src={youtubeVideoString} allowFullScreen="true" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" ></iframe>);
+            }
+        } else if (props.thumbnail != undefined) {
+            //else its an image and reurn image tag
+            return (<img id='portfolioVideoPlayer' alt="image" src={props.thumbnail}></img>);
         }
-        //could be a youtibe video 
-        if((props.videoLink).includes("youtube")){
-            let youtubeVideoString = (props.videoLink).replace("watch?v=", "embed/");
-            return(<iframe id='portfolioVideoPlayer' src={youtubeVideoString} allowFullScreen="true" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;" ></iframe>);
-        }
-        //else its an image and reurn image tag
-        return (<img id='portfolioVideoPlayer' alt="image" src={props.thumbnail}></img>);
+
+
     }
 
     return (
